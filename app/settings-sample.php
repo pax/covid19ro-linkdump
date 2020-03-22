f17e745a6517c1b42e962a8d3b80ea3e6bdc2c16
pax <pax@mioritics.ro>
2020-03-23 00:37:39 +0200

    cleanup

--- app/settings-sample.php
+++ app/settings-sample.php
@@ -1,25 +1,40 @@
 <?php
 
+// TODO: nicer const names
+
 /* fetch data 
 - - - - - - - - - - - - - - - - - - - - -  */
 
-$GLOBALS['zeFile'] = '../data/google-sheets.json';
-$GLOBALS['docID'] = '2PACX-1vTERxGzP9c65waSCL3Wskg2JDFi4GkIfC62uPIKo9Drxy5L46K1JvPFudehEEFd_gzIuIam74PDbwAs';
+$GLOBALS['docID'] = '2PACX-1vTpJvjP0NhF_6Ma2qOe-1Uy0FcD_qzNOhGAKSUMZNd6phzsgOw5Tm1bizZbAhUdtsoA2xkbdjzFbJDy';
 
+// TODO: automate this
 $GLOBALS['sheets'] = array(
-  'Info surse oficiale' => 1584676943,
-  'Solidaritate' => 0,
-  'Utile' => 333501499,
-  'Edutainment' => 1148678370
+  'Inițiative civice' => 0,
+  'Inițiative personale' => 1738822528,
+  'Inițiative companii' => 159026053,
+  'Educație Cursuri' => 1058734499,
+  'Cultură' => 1420865732,
+  'Donații prin ONG-uri' => 1732932632,
+  'Donații companii' => 1148538094
 );
 
+$GLOBALS['zeFile']      = '../data/google-sheets.json';
 
-/* render html 
+/* render html & fetch icons
 - - - - - - - - - - - - - - - - - - - - -  */
-
-$GLOBALS['sourceJson'] = '../data/google-sheets.json';
-$GLOBALS['targetfile'] = '../../index.html';
-$GLOBALS['iconsDIR'] = '../data/icons/';
+$GLOBALS['sourceJson']  = '../data/google-sheets.json';
+$GLOBALS['targetfile']  = '../../index.html';
+$GLOBALS['iconsDIR']    = '../data/icons/';
 $GLOBALS['iconsDIRrel'] = 'app/data/icons/';
 
- 
+/*  
+  TODO:
+  nav pre / post items
+  $GLOBALS['nav_pre'] 
+  $GLOBALS['nav_post'] 
+  $GLOBALS['widgets'] = [] 
+  $GLOBALS['footer'] 
+  $GLOBALS['header'] 
+  $GLOBALS['head'] 
+  $GLOBALS['og_image'] 
+*/
